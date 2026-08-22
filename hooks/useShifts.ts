66683740b87f0adacb9fec31fa0bb93aff14575a
Andrii <zahoruiko.andrii17@gmail.com>
2026-08-22 12:49:42 +0200

    done

--- hooks/useShifts.ts
+++ hooks/useShifts.ts
@@ -1,45 +1,55 @@
 import { getShifts } from "@/services/shiftsService";
-import { useShiftsStore } from "@/stores/shiftsStore";
 import { useQuery } from "@tanstack/react-query";
 
 export const useShifts = (startTime: Date, endTime: Date) => {
-  const pricePerHour = useShiftsStore((s) => s.pricePerHour);
+  const start = new Date(startTime);
+  const end = new Date(endTime);
+
+  start.setHours(0, 0, 0, 0);
+  end.setHours(23, 59, 59, 999);
+
   const shiftsListQuery = useQuery({
     queryKey: [
       "shifts",
       {
-        startTime: startTime.toISOString(),
-        endTime: endTime.toISOString(),
+        startTime: start.toISOString(),
+        endTime: end.toISOString(),
       },
     ],
     queryFn: () =>
       getShifts({
-        startTime: startTime.toISOString(),
-        endTime: endTime.toISOString(),
+        startTime: start.toISOString(),
+        endTime: end.toISOString(),
       }),
   });
 
-  const shifts = shiftsListQuery.data || [];
+  const shifts = shiftsListQuery.data ?? [];
 
-  const totalHours = shifts.reduce((acc, el) => {
-    return acc + el.totalHours;
+  const totalHours = shifts.reduce((acc, shift) => {
+    return acc + shift.totalHours;
   }, 0);
 
-  const nightHours = shifts.reduce((acc, el) => {
-    return acc + el.nightHours;
+  const nightHours = shifts.reduce((acc, shift) => {
+    return acc + shift.nightHours;
   }, 0);
 
   const dayHours = totalHours - nightHours;
 
-  const salary = shifts.reduce((acc, el) => {
-    const shiftDayHours = el.totalHours - el.nightHours;
+  const salary = shifts.reduce((acc, shift) => {
+    const shiftDayHours = shift.totalHours - shift.nightHours;
 
     const shiftSalary =
-      (el.nightHours / 60) * el.pricePerHour * 1.25 +
-      (shiftDayHours / 60) * el.pricePerHour;
+      (shift.nightHours / 60) * shift.pricePerHour * 1.25 +
+      (shiftDayHours / 60) * shift.pricePerHour;
 
     return acc + shiftSalary;
   }, 0);
 
-  return { shifts, totalHours, nightHours, salary, dayHours };
+  return {
+    shifts,
+    totalHours,
+    nightHours,
+    salary,
+    dayHours,
+  };
 };
