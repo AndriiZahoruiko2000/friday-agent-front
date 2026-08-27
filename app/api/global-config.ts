86d7be9a16b5aae3c16741805677de220278eec1
Andrii <zahoruiko.andrii17@gmail.com>
2026-08-27 11:49:02 +0200

    done

--- app/api/global-config.ts
+++ app/api/global-config.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export const globalAPI = axios.create({
-  baseURL: "http://localhost:3009",
+  baseURL: "https://0ljwn2puxb.execute-api.us-east-1.amazonaws.com",
   withCredentials: true,
 });
-
-// https://0ljwn2puxb.execute-api.us-east-1.amazonaws.com
